760f2f193c51465202814e965b03c84c6df748de
Cezary Androsiuk PC <czandros@wp.pl>
2026-05-19 09:37:47 +0200

    minor changes

--- source/InfiniterCore.h
+++ source/InfiniterCore.h
@@ -12,10 +12,10 @@ public:
     /// Constructor
     explicit InfiniterCore() noexcept;
 
-    InfiniterCore(uint64_t p_capacity); /// throws bad_alloc
+    explicit InfiniterCore(uint64_t p_capacity); /// throws bad_alloc
 
     /// value only covers first cell, purpose of this is to initialize instance with 1 or something like that
-    InfiniterCore(uint64_t p_capacity, uint64_t p_value, bool p_negative_value=false); /// throws bad_alloc
+    explicit InfiniterCore(uint64_t p_capacity, uint64_t p_value, bool p_negative_value=false); /// throws bad_alloc
     // czy opłaca się oznaczać konstruktory z uint64_t jako explicit?
     // tak samo w IM
 
